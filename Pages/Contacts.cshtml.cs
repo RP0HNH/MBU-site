3994a73f57b12eba1b36299687a84077d915d560
Alex <61711816+RP0HNH@users.noreply.github.com>
2025-12-09 15:31:15 +0300

    Добавьте файлы проекта.

---
+++ Pages/Contacts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MBU_site.Pages
+{
+    public class ContactsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
