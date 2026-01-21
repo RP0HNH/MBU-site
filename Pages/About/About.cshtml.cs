76176edd929eb616a4032e246fa2c01ac4a46338
Alex <61711816+RP0HNH@users.noreply.github.com>
2026-01-21 08:33:48 +0300

    О компании

---
+++ Pages/About/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MBU_site.Pages.About
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
