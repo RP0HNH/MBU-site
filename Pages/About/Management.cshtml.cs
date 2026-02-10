971408010c5a09a89328bbd4ba278cb65404e197
Alex <61711816+RP0HNH@users.noreply.github.com>
2026-02-10 22:29:25 +0300

    Добавление новых разделов

---
+++ Pages/About/Management.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MBU_site.Pages.About
+{
+    public class ManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
